43ab3a072eda602af9113c57b937b6236a09c3bd
Jimish Fotariya <fotariyajimish@gmail.com>
2024-01-09 19:47:25 +0530

    feat: test again with mod

--- examples/complete/dashboard.tf
+++ examples/complete/dashboard.tf
@@ -3,7 +3,7 @@ locals {
   light_colors = ["#ff7979", "#87CEFA", "#97a397", "#ffa07a", "#f9e886", "#b0fafa", "#d8fba4", "#f8aff4"]
   dark_colors  = ["#db041d", "#0c79e6", "#015101", "#e45f2e", "#dfa104", "#06b3b6", "#62c203", "#c50e97"]
 
-  alarm_names = keys(aws_cloudwatch_metric_alarm.custom_alarms)
+  alarm_names        = keys(aws_cloudwatch_metric_alarm.custom_alarms)
   metrics_data_point = [
     for i, alarm_key in local.alarm_names :
     [
@@ -38,7 +38,14 @@ locals {
     ]
   ]
 
-  metrics_combined = concat(local.metrics_data_point, local.metrics_data_fill)
+  #    metrics_combined = concat(local.metrics_data_point, local.metrics_data_fill)
+
+  metrics_combined = [
+    for i in range(length(local.metrics_data_point) * 2) : (i % 2 == 0 ? local.metrics_data_point[floor(i % 2)] : local
+    .metrics_data_fill[
+    floor(i % 2)
+    ])
+  ]
 }
 
 
@@ -60,9 +67,9 @@ module "alarm_history_dashboard" {
 
   additional_widgets = [
     {
-      type   = "metric"
-      height = 6
-      width  = 18
+      type       = "metric"
+      height     = 6
+      width      = 18
       properties = {
         metrics = local.metrics_combined
         period  = 10
@@ -71,7 +78,7 @@ module "alarm_history_dashboard" {
         title   = "Alarm History"
         view    = "timeSeries"
         stacked = true,
-        legend = {
+        legend  = {
           position = "right"
         }
         yAxis = {
