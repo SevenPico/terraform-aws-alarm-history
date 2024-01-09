7879606fe548529b0cd415360b3283ce8c91e1ad
Jimish Fotariya <fotariyajimish@gmail.com>
2024-01-09 17:55:51 +0530

    fix: changed loop to use alarm_keys

--- examples/complete/dashboard.tf
+++ examples/complete/dashboard.tf
@@ -3,10 +3,10 @@ locals {
   light_colors = ["#ff7979", "#87CEFA", "#97a397", "#ffa07a", "#f9e886", "#b0fafa", "#d8fba4", "#f8aff4"]
   dark_colors  = ["#db041d", "#0c79e6", "#015101", "#e45f2e", "#dfa104", "#06b3b6", "#62c203", "#c50e97"]
 
-  alarm_names = keys(aws_cloudwatch_metric_alarm.custom_alarms)
+  alarm_keys = keys(aws_cloudwatch_metric_alarm.custom_alarms)
 
   metrics_combined = [
-    for i in range(length(local.keys) * 2) :
+    for i in range(length(local.alarm_keys) * 2) :
     (i % 2 == 0 ? [
       var.metric_namespace,
       local.metric_name,
@@ -15,7 +15,7 @@ locals {
       var.metric_service_name,
 
       "Alarm Name",
-      aws_cloudwatch_metric_alarm.custom_alarms[local.keys[i]].alarm_name,
+      aws_cloudwatch_metric_alarm.custom_alarms[local.alarm_keys[i]].alarm_name,
 
       {
         id : "m${i}",
@@ -26,7 +26,7 @@ locals {
     [
       {
         expression : "FILL(m${i}, REPEAT)",
-        label : "FILL: ${aws_cloudwatch_metric_alarm.custom_alarms[local.keys[i]].alarm_name}"
+        label : "FILL: ${aws_cloudwatch_metric_alarm.custom_alarms[local.alarm_keys[i]].alarm_name}"
         id : "e${i}"
         region : var.region
         period : 10
