24222c23f069db94485e82491a6172e75681d494
Jimish Fotariya <fotariyajimish@gmail.com>
2024-01-09 20:44:36 +0530

    wip: using map

--- examples/complete/dashboard.tf
+++ examples/complete/dashboard.tf
@@ -3,45 +3,49 @@ locals {
   light_colors = ["#ff7979", "#87CEFA", "#97a397", "#ffa07a", "#f9e886", "#b0fafa", "#d8fba4", "#f8aff4"]
   dark_colors  = ["#db041d", "#0c79e6", "#015101", "#e45f2e", "#dfa104", "#06b3b6", "#62c203", "#c50e97"]
 
-  alarm_names = keys(aws_cloudwatch_metric_alarm.custom_alarms)
+  alarm_names        = keys(aws_cloudwatch_metric_alarm.custom_alarms)
   metrics_data_point = [
     for i, alarm_key in local.alarm_names :
-    { value = [
-      var.metric_namespace,
-      local.metric_name,
+    {
+      value = [
+        var.metric_namespace,
+        local.metric_name,
 
-      "service",
-      var.metric_service_name,
+        "service",
+        var.metric_service_name,
 
-      "Alarm Name",
-      aws_cloudwatch_metric_alarm.custom_alarms[alarm_key].alarm_name,
+        "Alarm Name",
+        aws_cloudwatch_metric_alarm.custom_alarms[alarm_key].alarm_name,
 
-      {
-        id : "m${i}",
-        region : var.region,
-        color : local.dark_colors[i]
-      }
-    ] }
+        {
+          id : "m${i}",
+          region : var.region,
+          color : local.dark_colors[i]
+        }
+      ]
+    }
   ]
 
   metrics_data_fill = [
     for i, alarm_key in local.alarm_names :
-    { value = [
-      {
-        expression : "FILL(m${i}, REPEAT)",
-        label : "FILL: ${aws_cloudwatch_metric_alarm.custom_alarms[alarm_key].alarm_name}"
-        id : "e${i}"
-        region : var.region
-        period : 10
-        color : local.light_colors[i]
-      }
-    ] }
+    {
+      value = [
+        {
+          expression : "FILL(m${i}, REPEAT)",
+          label : "FILL: ${aws_cloudwatch_metric_alarm.custom_alarms[alarm_key].alarm_name}"
+          id : "e${i}"
+          region : var.region
+          period : 10
+          color : local.light_colors[i]
+        }
+      ]
+    }
   ]
 
   #  metrics_combined = concat(local.metrics_data_point, local.metrics_data_fill)
   metrics_combined = [
     for i in range(length(local.metrics_data_point) * 2) :
-    (i % 2 == 0 ? local.metrics_data_point[floor(i / 2)].value : local.metrics_data_fill[floor(i / 2)].value)
+    (i % 2 == 0 ? local.metrics_data_point[floor(i / 2)] : local.metrics_data_fill[floor(i / 2)])
   ]
 }
 
@@ -63,18 +67,18 @@ module "alarm_history_dashboard" {
 
   additional_widgets = [
     {
-      type   = "metric"
-      height = 6
-      width  = 18
+      type       = "metric"
+      height     = 6
+      width      = 18
       properties = {
-        metrics = local.metrics_combined
+        metrics = [for m in local.metrics_combined : m["value"]]
         period  = 10
         stat    = "Maximum"
         region  = local.region
         title   = "Alarm History"
         view    = "timeSeries"
         stacked = true,
-        legend = {
+        legend  = {
           position = "right"
         }
         yAxis = {
