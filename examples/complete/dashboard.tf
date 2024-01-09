6335302e3143a656462892bd65e75b2740beed6d
Jimish Fotariya <fotariyajimish@gmail.com>
2024-01-09 17:48:26 +0530

    wip: test a merge using integer devision, moved all to combined metrics array

--- examples/complete/dashboard.tf
+++ examples/complete/dashboard.tf
@@ -4,9 +4,10 @@ locals {
   dark_colors  = ["#db041d", "#0c79e6", "#015101", "#e45f2e", "#dfa104", "#06b3b6", "#62c203", "#c50e97"]
 
   alarm_names = keys(aws_cloudwatch_metric_alarm.custom_alarms)
-  metrics_data_point = [
-    for i, alarm_key in local.alarm_names :
-    [
+
+  metrics_combined = [
+    for i in range(length(local.keys) * 2) :
+    (i % 2 == 0 ? [
       var.metric_namespace,
       local.metric_name,
 
@@ -14,41 +15,24 @@ locals {
       var.metric_service_name,
 
       "Alarm Name",
-      aws_cloudwatch_metric_alarm.custom_alarms[alarm_key].alarm_name,
+      aws_cloudwatch_metric_alarm.custom_alarms[local.keys[i]].alarm_name,
 
       {
         id : "m${i}",
         region : var.region,
         color : local.dark_colors[i]
       }
-    ]
-  ]
-
-  metrics_data_fill = [
-    for i, alarm_key in local.alarm_names :
+    ] :
     [
       {
         expression : "FILL(m${i}, REPEAT)",
-        label : "FILL: ${aws_cloudwatch_metric_alarm.custom_alarms[alarm_key].alarm_name}"
+        label : "FILL: ${aws_cloudwatch_metric_alarm.custom_alarms[local.keys[i]].alarm_name}"
         id : "e${i}"
         region : var.region
         period : 10
         color : local.light_colors[i]
       }
-    ]
-  ]
-
-  metrics_combined = [
-    #    for i in range(length(local.keys) * 2) :
-    #    (i % 2 == 0 ? local.metrics_data_point[floor(i / 2)] : local.metrics_data_fill[floor(i / 2)])
-  ]
-  #  metrics_combined = sort(concat(metrics_data_point, metrics_data_fill))
-}
-
-output "loop_results" {
-  value = [
-    for i in range(length(local.keys) * 2) :
-    [i % 2 == 0 ? local.metrics_data_point[floor(i / 2)] : local.metrics_data_fill[floor(i / 2)]]
+    ])
   ]
 }
 
@@ -70,9 +54,9 @@ module "alarm_history_dashboard" {
 
   additional_widgets = [
     {
-      type   = "metric"
-      height = 6
-      width  = 18
+      type       = "metric"
+      height     = 6
+      width      = 18
       properties = {
         metrics = local.metrics_combined
         period  = 300
@@ -89,3 +73,4 @@ module "alarm_history_dashboard" {
   ]
 }
 
+output "loop_results" {
